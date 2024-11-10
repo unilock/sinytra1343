8b1fea03d6126373065a08eb1e1414c004592840
unilock <unilock@fennet.rentals>
2024-11-10 06:11:06 -0500

    slightly better fix + account for NAMETAG_DISTANCE and CREATIVE_FLIGHT
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/sinytra1343/mixin/LivingEntityMixin.java
+++ src/main/java/cc/unilock/sinytra1343/mixin/LivingEntityMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.core.Holder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.common.NeoForgeMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +13,19 @@ import org.spongepowered.asm.mixin.injection.At;
 public class LivingEntityMixin {
 	@ModifyReturnValue(method = "getAttributeValue", at = @At("RETURN"))
 	private double getAttributeValue(double original, Holder<Attribute> attribute) {
-		return attribute.is(NeoForgeMod.SWIM_SPEED::is) ? 1.0D : original;
+		if (original == 0.0D) {
+			if (attribute.is(NeoForgeMod.SWIM_SPEED::is)) {
+				return 1.0D;
+			}
+			if (attribute.is(NeoForgeMod.NAMETAG_DISTANCE::is)) {
+				return 64.0D;
+			}
+			if (attribute.is(NeoForgeMod.CREATIVE_FLIGHT::is)) {
+				if (((LivingEntity) (Object) this) instanceof Player player && player.isCreative()) {
+					return 1.0D;
+				}
+			}
+		}
+		return original;
 	}
 }
